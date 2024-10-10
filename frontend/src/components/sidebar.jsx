f46c4ba976299366af769a3d8345a3dfebef6161
kpan13 <kajaljpan@gmail.com>
2024-10-10 14:26:10 -0700

    add sidebar.jsx and home.jsx

---
+++ frontend/src/components/sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="flex flex-col">
+            <div>
+               <h2 className="text-xl font-semibold"> Title </h2>
+               <h4 className="my-2 text-gray-400">Email</h4>
+               <hr/>
+            </div>
+        </div>
+    );
+};
+
+     export default Sidebar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '../components/sidebar';
+
+const Home = () => {
+    return (
+        <div className="flex h-[98vh] gap-4">
+            <div className="bg-pink-100 w-2/5 ">hello world</div>
+            <div className="bg-black-200">hello world</div>
+        </div>
+    ); 
+};
+
+      export default Home;
